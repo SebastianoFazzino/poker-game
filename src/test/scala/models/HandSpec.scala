a62b1a085db605078833643d3fa0bdfa12ecfcb1
Seb <sebastiano.fazzino@swagyourlife.com>
2024-07-12 19:23:29 +0300

    added multiple tests for compare twoPair

--- src/test/scala/models/HandSpec.scala
+++ src/test/scala/models/HandSpec.scala
@@ -19,6 +19,29 @@ class HandSpec extends AnyFlatSpec with Matchers {
     hand.evaluateHand shouldEqual HandScore.Flush
   }
 
+  // Check if compareFlush returns the correct comparison result
+
+  it should "compare flushes by suit" in {
+    val hand1Cards = List(
+      new Card("Hearts", "2"),
+      new Card("Hearts", "4"),
+      new Card("Hearts", "6"),
+      new Card("Hearts", "8"),
+      new Card("Hearts", "10")
+    )
+    val hand2Cards = List(
+      new Card("Diamonds", "2"),
+      new Card("Diamonds", "4"),
+      new Card("Diamonds", "6"),
+      new Card("Diamonds", "8"),
+      new Card("Diamonds", "10")
+    )
+    val hand1 = new Hand(hand1Cards)
+    val hand2 = new Hand(hand2Cards)
+
+    hand1.compareHands(hand2) shouldEqual 1 // Hearts flush beats Diamonds flush
+  }
+
   it should "evaluate to a straight" in {
     val cards = List(
       new Card("Hearts", "2"),
@@ -94,6 +117,54 @@ class HandSpec extends AnyFlatSpec with Matchers {
     )
     val hand = new Hand(cards)
     hand.evaluateHand shouldEqual HandScore.TwoPairs
+  }
+
+  it should "compare hands with Two Pairs correctly" in {
+    val hand1 = new Hand(List(
+      new Card("Hearts", "3"),
+      new Card("Diamonds", "3"),
+      new Card("Clubs", "5"),
+      new Card("Spades", "5"),
+      new Card("Hearts", "6")
+    ))
+    val hand2 = new Hand(List(
+      new Card("Hearts", "2"),
+      new Card("Diamonds", "2"),
+      new Card("Clubs", "King"),
+      new Card("Spades", "King"),
+      new Card("Hearts", "Ace")
+    ))
+
+    val hand3 = new Hand(List(
+      new Card("Hearts", "4"),
+      new Card("Diamonds", "4"),
+      new Card("Clubs", "6"),
+      new Card("Spades", "6"),
+      new Card("Hearts", "3")
+    ))
+
+    hand2.compareHands(hand1) shouldEqual 1 // Two Pairs (Kings and 2s) beats Two Pairs (5s and 3s)
+    hand3.compareHands(hand2) shouldEqual -1 // Two Pairs (6s and 4s) loses to Two Pairs (Kings and 2s)
+    hand3.compareHands(hand1) shouldEqual 1 // Two Pairs (6s and 4s) beats Two Pairs (5s and 3s)
+
+    // Check if the higher card is considered when comparing two pairs
+    val hand4 = new Hand(List(
+      new Card("Hearts", "4"),
+      new Card("Diamonds", "4"),
+      new Card("Clubs", "6"),
+      new Card("Spades", "6"),
+      new Card("Diamonds", "2")
+    ))
+
+    val hand5 = new Hand(List(
+      new Card("Clubs", "4"),
+      new Card("Spades", "4"),
+      new Card("Hearts", "6"),
+      new Card("Clubs", "6"),
+      new Card("Hearts", "3")
+    ))
+
+    hand4.compareHands(hand5) shouldEqual -1 // Two Pairs (6s and 4s, kicker 2) loses against Two Pairs (6s and 4s, kicker 3)
   }
 
   it should "evaluate to one pair" in {
@@ -174,28 +245,5 @@ class HandSpec extends AnyFlatSpec with Matchers {
       new Card("Hearts", "10")
     ))
     onePairHand.compareHands(highCardHand) shouldEqual 1 // One pair beats high card
-  }
-
-  // Check if compareFlush returns the correct comparison result
-
-  it should "compare flushes by suit" in {
-    val hand1Cards = List(
-      new Card("Hearts", "2"),
-      new Card("Hearts", "4"),
-      new Card("Hearts", "6"),
-      new Card("Hearts", "8"),
-      new Card("Hearts", "10")
-    )
-    val hand2Cards = List(
-      new Card("Diamonds", "2"),
-      new Card("Diamonds", "4"),
-      new Card("Diamonds", "6"),
-      new Card("Diamonds", "8"),
-      new Card("Diamonds", "10")
-    )
-    val hand1 = new Hand(hand1Cards)
-    val hand2 = new Hand(hand2Cards)
-
-    hand1.compareHands(hand2) shouldEqual 1 // Hearts flush beats Diamonds flush
   }
 }
